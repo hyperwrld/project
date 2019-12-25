87addc3849f97255c4e1b94fe8e0410954a934ca
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2019-12-25 16:47:30 +0000

    displayed job name on the character selection

--- resources/[cacemrp]/crp-login/html/js/main.js
+++ resources/[cacemrp]/crp-login/html/js/main.js
@@ -4,8 +4,8 @@ var isWaiting = false;
 var LoadingMessages = new Array('Aguarde enquanto metemos carvão na máquina &#128642;', 'Aguarde enquanto estamos a carregar a torradeira &#128012;');
 
 
-$(function() {
-	defaultHTML = $('.main-container').clone();
+$(function () {
+    defaultHTML = $('.main-container').clone();
 
     var cleaveDate = new Cleave('.input-date', {
         date: true,
@@ -19,34 +19,34 @@ $(function() {
 
     CreateSpawnButton();
 
-    window.addEventListener('message', function(event) {
+    window.addEventListener('message', function (event) {
         if (event.data.open == true) {
             ShowHUD();
         } else if (event.data.reload == true) {
             isWaiting = false;
 
             $('.main-container').replaceWith(defaultHTML.clone());
-            $('.main-container').css({'display':'none'});
-            $('.register-container').css({'display':'none'});
+            $('.main-container').css({ 'display': 'none' });
+            $('.register-container').css({ 'display': 'none' });
 
             $('#form').trigger('reset');
             $('#register-form').scrollTop(0);
 
-            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters : true, showcursor : true })); 
-       	} else if (event.data.close == true) {
-       		$('.main-container').replaceWith(defaultHTML.clone());
+            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters: true, showcursor: true }));
+        } else if (event.data.close == true) {
+            $('.main-container').replaceWith(defaultHTML.clone());
 
-            $('.main-container').css({'display':'none'});
-            $('.register-container').css({'display':'none'});
-            $('.spawn-container').css({'display':'none'});
-            
-            $('#disconnect').css({'display':'none'});
+            $('.main-container').css({ 'display': 'none' });
+            $('.register-container').css({ 'display': 'none' });
+            $('.spawn-container').css({ 'display': 'none' });
+
+            $('#disconnect').css({ 'display': 'none' });
         } else if (event.data.error == true) {
-           TriggerErrorNotification(event.data.message);
+            TriggerErrorNotification(event.data.message);
         } else if (event.data.playercharacters || event.data.playercharacters == undefined) {
             isWaiting = false;
 
-            setTimeout(function(){ showSelection(event.data); }, 5000);
+            setTimeout(function () { showSelection(event.data); }, 5000);
         }
     });
 });
@@ -56,24 +56,24 @@ function ShowHUD() {
 
     disconnect.setAttribute('style', 'display: block;');
 
-    disconnect.addEventListener('click', function() { 
-    	$.post('http://crp-login/nuiMessage', JSON.stringify({ disconnect : true })); 
+    disconnect.addEventListener('click', function () {
+        $.post('http://crp-login/nuiMessage', JSON.stringify({ disconnect: true }));
     }, false);
 
-    $('.login-container').css({'display':'block'});
+    $('.login-container').css({ 'display': 'block' });
     $('.load-txt').show();
 
     DesactivateLoading();
 
-    document.onkeyup = function(data) {
+    document.onkeyup = function (data) {
         if (data.which == 13) { // ENTER
             $('.load-txt').hide();
 
-            $('.login-container').css({'display':'none'});
+            $('.login-container').css({ 'display': 'none' });
 
             ActivateLoading();
 
-            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters : true, showcursor : true })); 
+            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters: true, showcursor: true }));
             document.onkeyup = null;
         }
     };
@@ -87,28 +87,29 @@ function showSelection(data) {
     $('.main-container').show();
 
     if (Array.isArray(data.playercharacters) && data.playercharacters.length) {
-        $.each(data.playercharacters, function(index, char){
+        $.each(data.playercharacters, function (index, char) {
             id = id + 1;
 
             var charactername = document.getElementById('charid-' + id).getElementsByClassName('character-n')[0];
             var characterinfo = document.getElementById('charid-' + id).getElementsByClassName('character-detail')[0];
-            var playbutton    = document.getElementById('charid-' + id).getElementsByClassName('btn-play btn-block')[0];
-            var deletebutton  = document.getElementById('charid-' + id).getElementsByClassName('btn-delete btn-block')[0];
-            var createbutton  = document.getElementById('charid-' + id).getElementsByClassName('btn-create btn-block')[0];
+            var playbutton = document.getElementById('charid-' + id).getElementsByClassName('btn-play btn-block')[0];
+            var deletebutton = document.getElementById('charid-' + id).getElementsByClassName('btn-delete btn-block')[0];
+            var createbutton = document.getElementById('charid-' + id).getElementsByClassName('btn-create btn-block')[0];
 
             createbutton.setAttribute('style', 'display: none;');
 
             charactername.textContent = char.firstname + ' ' + char.lastname;
-            characterinfo.innerHTML   = '</br><b>Sexo:</b></br>' + char.sex + '</br></br><b>Data de Nascimento:</b></br>' + char.dateofbirth + '</br></br><b>Profissão:</b></br>' + char.job + '</br></br><b>Dinheiro:</b></br>' + char.money + ' €</br></br><b>Banco:</b></br>' + char.bank + ' €</br></br><b>História:</b></br>' + char.history;    
+
+            characterinfo.innerHTML = '</br><b>Sexo:</b></br>' + char.sex + '</br></br><b>Data de Nascimento:</b></br>' + char.dateofbirth + '</br></br><b>Profissão:</b></br>' + JSON.parse(char.job).label + '</br></br><b>Dinheiro:</b></br>' + char.money + ' €</br></br><b>Banco:</b></br>' + char.bank + ' €</br></br><b>História:</b></br>' + char.history;
 
             SetElementState(playbutton, deletebutton, 'block', 'display');
 
-            playbutton.addEventListener('click', function() {
+            playbutton.addEventListener('click', function () {
                 console.log(char.id)
-                $.post('http://crp-login/nuiMessage', JSON.stringify({ selectcharacter : true, character : char.id, showcursor : true}));
+                $.post('http://crp-login/nuiMessage', JSON.stringify({ selectcharacter: true, character: char.id, showcursor: true }));
             }, false);
 
-            deletebutton.addEventListener('click', function() {
+            deletebutton.addEventListener('click', function () {
                 charactername.textContent = 'Deseja eliminar a personagem?';
 
                 SetElementState(deletebutton, playbutton, 'none', 'display');
@@ -117,15 +118,15 @@ function showSelection(data) {
             }, false);
 
             if (data.playercharacters.length == id) {
-                for (i = (id + 1) ; i < 7; i++) { 
+                for (i = (id + 1); i < 7; i++) {
                     var createbutton = document.getElementById('charid-' + i).getElementsByClassName('btn-create btn-block')[0];
 
                     createbutton.setAttribute('style', 'display: block;');
 
-                    createbutton.addEventListener('click', function() {
+                    createbutton.addEventListener('click', function () {
                         var characterContainer = document.getElementsByClassName('register-container')[0];
 
-                        $('.main-container').css({'display':'none'});
+                        $('.main-container').css({ 'display': 'none' });
 
                         characterContainer.setAttribute('style', 'display: block;');
                     }, false);
@@ -133,15 +134,15 @@ function showSelection(data) {
             }
         });
     } else {
-        for (i = 1 ; i < 7; i++) { 
+        for (i = 1; i < 7; i++) {
             var createbutton = document.getElementById('charid-' + i).getElementsByClassName('btn-create btn-block')[0];
 
             createbutton.setAttribute('style', 'display: block;');
 
-            createbutton.addEventListener('click', function() {
+            createbutton.addEventListener('click', function () {
                 var characterContainer = document.getElementsByClassName('register-container')[0];
 
-                $('.main-container').css({'display':'none'});
+                $('.main-container').css({ 'display': 'none' });
 
                 characterContainer.setAttribute('style', 'display: block;');
             }, false);
@@ -150,7 +151,7 @@ function showSelection(data) {
 }
 
 function CreateCharacterButton() {
-    $('#createchar').click(function(event){
+    $('#createchar').click(function (event) {
         event.preventDefault();
 
         if (isWaiting == false) {
@@ -166,47 +167,47 @@ function CreateCharacterButton() {
                 return;
             }
 
-            $.post('http:/crp-login/nuiMessage', JSON.stringify({ 
-                createcharacter : true, 
-                showcursor : true,
-                chardata : { 
-                    firstname: $('#firstname').val(), 
-                    lastname: $('#lastname').val(), 
-                    dateofbirth: $('#dateofbirth').val(), 
-                    sex: $('#sex').val(), 
+            $.post('http:/crp-login/nuiMessage', JSON.stringify({
+                createcharacter: true,
+                showcursor: true,
+                chardata: {
+                    firstname: $('#firstname').val(),
+                    lastname: $('#lastname').val(),
+                    dateofbirth: $('#dateofbirth').val(),
+                    sex: $('#sex').val(),
                     history: history
                 }
             }));
         }
     });
 
-    $('#backcharlist').click(function(event){
+    $('#backcharlist').click(function (event) {
         event.preventDefault();
 
         if (isWaiting == false) {
             isWaiting = true;
 
             $('.main-container').replaceWith(defaultHTML.clone());
-            $('.main-container').css({'display':'none'});
-            $('.register-container').css({'display':'none'});
+            $('.main-container').css({ 'display': 'none' });
+            $('.register-container').css({ 'display': 'none' });
 
             $('#form').trigger('reset');
             $('#register-form').scrollTop(0);
 
-            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters : true, showcursor : true }));
+            $.post('http://crp-login/nuiMessage', JSON.stringify({ fetchcharacters: true, showcursor: true }));
         }
-    });    
+    });
 }
 
 function CreateSpawnButton() {
-	$('.btn-loc').click(function(event){
-		console.log($(this).attr('data-spawnid'))
+    $('.btn-loc').click(function (event) {
+        console.log($(this).attr('data-spawnid'))
 
-		$.post('http:/crp-login/nuiMessage', JSON.stringify({
-			selectedspawn : true,
-			spawnid       : $(this).attr('data-spawnid') 
-		}));
-	});
+        $.post('http:/crp-login/nuiMessage', JSON.stringify({
+            selectedspawn: true,
+            spawnid: $(this).attr('data-spawnid')
+        }));
+    });
 }
 
 function TriggerErrorNotification(message) {
@@ -223,20 +224,20 @@ function TriggerErrorNotification(message) {
 }
 
 function ShowConfirmationButtons(parent, char, data) {
-    var parentID  = parent.getAttribute('id');
+    var parentID = parent.getAttribute('id');
     var yesbutton = document.getElementById(parentID).getElementsByClassName('btn-delyes btn-block')[0];
-    var nobutton  = document.getElementById(parentID).getElementsByClassName('btn-delno btn-block')[0];
+    var nobutton = document.getElementById(parentID).getElementsByClassName('btn-delno btn-block')[0];
 
     SetElementState(yesbutton, nobutton, 'block', 'display');
 
-    yesbutton.addEventListener('click', function() {
+    yesbutton.addEventListener('click', function () {
         $.post('http://crp-login/nuiMessage', JSON.stringify({
             deletecharacter: char,
-            showcursor : true,
+            showcursor: true,
         }));
     }, false);
 
-    nobutton.addEventListener('click', function() {
+    nobutton.addEventListener('click', function () {
         SetElementState(yesbutton, nobutton, 'none', 'display');
 
         showSelection(data);
@@ -245,13 +246,13 @@ function ShowConfirmationButtons(parent, char, data) {
 
 function SetElementState(element1, element2, state, style) {
     element1.setAttribute('style', style + ': ' + state + ';');
-    element2.setAttribute('style', style + ': ' + state + ';');  
+    element2.setAttribute('style', style + ': ' + state + ';');
 }
 
 function ActivateLoading() {
     $('.loading').show();
 
-    $('.loading-txt').html(LoadingMessages[Math.floor( Math.random() * LoadingMessages.length )]);
+    $('.loading-txt').html(LoadingMessages[Math.floor(Math.random() * LoadingMessages.length)]);
 }
 
 function DesactivateLoading() {
