6adac6727bdbbfbd8bd673a13a60fc2de3879c51
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-01-02 19:47:07 +0000

    added one chat template

--- resources/[system]/chat/html/config.js
+++ resources/[system]/chat/html/config.js
@@ -5,7 +5,8 @@ window.CONFIG = {
         'default': '<b>{0}</b>: {1}',
         'defaultAlt': '{0}',
         'orange': '<div style="padding: 0.4vw; margin: 0.3vw; background-color: rgba(249, 176, 21, 0.7); border-radius: 4px;"></i> <b>{0}:</b> {1}</div>',
-        'blue': '<div style="padding: 0.4vw; margin: 0.3vw; background-color: rgba(28, 160, 242, 0.7); border-radius: 4px;"></i> <b>{0}:</b> {1}</div>'
+        'blue': '<div style="padding: 0.4vw; margin: 0.3vw; background-color: rgba(28, 160, 242, 0.7); border-radius: 4px;"></i> <b>{0}:</b> {1}</div>',
+        'red': '<div style="padding: 0.4vw; margin: 0.3vw; background-color: rgba(163, 44, 44, 0.7); border-radius: 4px;"></i> <b>{0}:</b> {1}</div>',
     },
     fadeTimeout: 2000,
     suggestionLimit: 5,
