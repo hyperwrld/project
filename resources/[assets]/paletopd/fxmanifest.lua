ff7de5a7d3151fe6edcf17d32b736c466b7ac99a
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-01-22 22:26:06 +0000

    removed some scripts

---
+++ resources/[assets]/paletopd/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+files {
+    'interiorproxies.meta'
+}
