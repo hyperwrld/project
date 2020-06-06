1d1e9961fc2899f87b2d06ff565b9f98dfa11952
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-06-06 11:05:35 +0100

    update: ghmattimysql

--- resources/[essentials]/ghmattimysql/fxmanifest.lua
+++ resources/[essentials]/ghmattimysql/fxmanifest.lua
@@ -1,31 +1,36 @@
 fx_version 'adamant'
+game 'common'
 
-game 'gta5'
+name 'ghmattimysql'
+description 'MySQL Middleware for fivem using mysql.js.'
+author 'Matthias Mandelartz'
+version '1.2.3'
+url 'https://github.com/GHMatti/ghmattimysql'
 
-ui_page 'ui/index.html'
+server_scripts {
+  'ghmattimysql-server.js',
+  'ghmattimysql-server.lua',
+}
 
 client_script 'ghmattimysql-client.js'
 
-server_scripts {
-    'ghmattimysql-server.js',
-    'ghmattimysql-server.lua',
+files {
+  'ui/index.html',
+  'ui/app.js',
+  'ui/app.css',
+  'ui/app.css',
+  'ui/fonts/alegreya-sans-v9-latin-300.woff',
+  'ui/fonts/alegreya-sans-v9-latin-500.woff',
+  'ui/fonts/alegreya-sans-v9-latin-700.woff',
+  'ui/fonts/alegreya-sans-v9-latin-regular.woff',
+  'ui/fonts/alegreya-sans-v9-latin-300.woff2',
+  'ui/fonts/alegreya-sans-v9-latin-500.woff2',
+  'ui/fonts/alegreya-sans-v9-latin-700.woff2',
+  'ui/fonts/alegreya-sans-v9-latin-regular.woff2',
+  'ui/fonts/MaterialIcons-Regular.eot',
+  'ui/fonts/MaterialIcons-Regular.ttf',
+  'ui/fonts/MaterialIcons-Regular.woff',
+  'ui/fonts/MaterialIcons-Regular.woff2',
 }
 
-files {
-    'ui/index.html',
-    'ui/app.js',
-    'ui/app.css',
-    'ui/app.css',
-    'ui/fonts/alegreya-sans-v9-latin-300.woff',
-    'ui/fonts/alegreya-sans-v9-latin-500.woff',
-    'ui/fonts/alegreya-sans-v9-latin-700.woff',
-    'ui/fonts/alegreya-sans-v9-latin-regular.woff',
-    'ui/fonts/alegreya-sans-v9-latin-300.woff2',
-    'ui/fonts/alegreya-sans-v9-latin-500.woff2',
-    'ui/fonts/alegreya-sans-v9-latin-700.woff2',
-    'ui/fonts/alegreya-sans-v9-latin-regular.woff2',
-    'ui/fonts/MaterialIcons-Regular.eot',
-    'ui/fonts/MaterialIcons-Regular.ttf',
-    'ui/fonts/MaterialIcons-Regular.woff',
-    'ui/fonts/MaterialIcons-Regular.woff2',
-}
+ui_page 'ui/index.html'
