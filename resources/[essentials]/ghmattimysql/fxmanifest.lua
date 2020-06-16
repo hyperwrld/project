3b477dccec9c02eb13429c3f78e7edfd0705a1a8
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-06-16 14:01:46 +0100

    [update]: Updated ghmattimysql to a newer version

--- resources/[essentials]/ghmattimysql/fxmanifest.lua
+++ resources/[essentials]/ghmattimysql/fxmanifest.lua
@@ -1,36 +1,22 @@
-fx_version 'adamant'
+fx_version 'bodacious'
+
 game 'common'
 
-name 'ghmattimysql'
-description 'MySQL Middleware for fivem using mysql.js.'
-author 'Matthias Mandelartz'
-version '1.2.3'
-url 'https://github.com/GHMatti/ghmattimysql'
-
 server_scripts {
-  'ghmattimysql-server.js',
-  'ghmattimysql-server.lua',
+    'ghmattimysql-server.js',
+    'ghmattimysql-server.lua',
 }
 
 client_script 'ghmattimysql-client.js'
 
 files {
-  'ui/index.html',
-  'ui/app.js',
-  'ui/app.css',
-  'ui/app.css',
-  'ui/fonts/alegreya-sans-v9-latin-300.woff',
-  'ui/fonts/alegreya-sans-v9-latin-500.woff',
-  'ui/fonts/alegreya-sans-v9-latin-700.woff',
-  'ui/fonts/alegreya-sans-v9-latin-regular.woff',
-  'ui/fonts/alegreya-sans-v9-latin-300.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-500.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-700.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-regular.woff2',
-  'ui/fonts/MaterialIcons-Regular.eot',
-  'ui/fonts/MaterialIcons-Regular.ttf',
-  'ui/fonts/MaterialIcons-Regular.woff',
-  'ui/fonts/MaterialIcons-Regular.woff2',
+    'ui/index.html',
+    'ui/js/app.js',
+    'ui/css/app.css',
+    'ui/fonts/*.woff',
+    'ui/fonts/*.woff2',
+    'ui/fonts/*.eot',
+    'ui/fonts/*.ttf',
 }
 
 ui_page 'ui/index.html'
