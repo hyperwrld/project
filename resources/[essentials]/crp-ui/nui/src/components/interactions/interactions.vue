1c9f8d48ebf7ce10467d74d3f944436179863e89
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-08-28 04:46:41 +0100

    [feat]: added interactions component and changed actionbar

---
+++ resources/[essentials]/crp-ui/nui/src/components/interactions/interactions.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container fluid>
+		<transition name='fade' :duration='1000'>
+			<v-alert v-if='interactions.status' class='interaction' dark prominent icon='fa-info-circle'>
+				{{ interactions.message }}
+			</v-alert>
+		</transition>
+    </v-container>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex';
+
+	import { library } from '@fortawesome/fontawesome-svg-core';
+	import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
+    export default {
+        name: 'interactions',
+        computed: {
+            ...mapGetters('app', ['interactions'])
+		}
+    };
+</script>
+
+<style scoped lang='scss'>
+    @import './interactions.scss';
+</style>
