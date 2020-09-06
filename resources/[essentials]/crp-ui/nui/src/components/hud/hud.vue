42458fad0e1400e1cf98934ef93ee725262e6475
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-09-06 14:42:03 +0100

    [feat]: some changes to the hud component.
    Changes:
    - Now than the player has 15 or less (health, hunger, thirst) the bar
    color turns red.
    - Fixed a bug where the status bars were starting from the top instead of
    the bottom.
    - Cleaned up the code a bit.

--- resources/[essentials]/crp-ui/nui/src/components/hud/hud.vue
+++ resources/[essentials]/crp-ui/nui/src/components/hud/hud.vue
@@ -12,31 +12,31 @@
 				</div>
 			</transition>
 		</div>
-        <div class='minimap' v-bind:style='{ top: minimapData.top, left: minimapData.left, width: minimapData.width, height: minimapData.height }'>
+        <div class='minimap' :style='{ top: minimapData.top, left: minimapData.left, width: minimapData.width, height: minimapData.height }'>
     		<div class='bar'>
     			<div class='health status'>
                     <font-awesome-icon icon='heart'></font-awesome-icon>
-    				<div class='sub-health' v-bind:style='{ width: minimapData.health }'></div>
+    				<div class='sub-health' :class='{ low: parseInt(minimapData.health) <= 15 }' :style='{ width: minimapData.health }'></div>
     			</div>
                 <div class='armour status'>
                     <font-awesome-icon icon='shield-alt'></font-awesome-icon>
-                    <div class='sub-armour' v-bind:style='{ width: minimapData.armour }'></div>
+                    <div class='sub-armour' :style='{ width: minimapData.armour }'></div>
                 </div>
                 <div class='hunger status'>
                     <font-awesome-icon icon='hamburger'></font-awesome-icon>
-                    <div class='sub-hunger' v-bind:style='{ height: minimapData.hunger, top: convertNumber(minimapData.hunger) }'></div>
+                    <div class='sub-hunger' :class='{ low: parseInt(minimapData.hunger) <= 15 }' :style='{ height: minimapData.hunger, top: convertNumber(minimapData.hunger) }'></div>
                 </div>
                 <div class='thirst status'>
                     <font-awesome-icon icon='tint'></font-awesome-icon>
-                    <div class='sub-thirst' v-bind:style='{ height: minimapData.thrist, top: convertNumber(minimapData.thrist) }'></div>
+                    <div class='sub-thirst' :class='{ low: parseInt(minimapData.thrist) <= 15 }' :style='{ height: minimapData.thrist, top: convertNumber(minimapData.thrist) }'></div>
                 </div>
                 <div class='breath status'>
                     <font-awesome-icon icon='lungs'></font-awesome-icon>
-                    <div class='sub-breath' v-bind:style='{ height: minimapData.breath, top: convertNumber(minimapData.breath) }'></div>
+                    <div class='sub-breath' :style='{ height: minimapData.breath, top: convertNumber(minimapData.breath) }'></div>
                 </div>
                 <div class='stress status'>
                     <font-awesome-icon icon='brain'></font-awesome-icon>
-                    <div class='sub-stress' v-bind:style='{ height: minimapData.stress, top: convertNumber(minimapData.stress) }'></div>
+                    <div class='sub-stress' :style='{ height: minimapData.stress, top: convertNumber(minimapData.stress) }'></div>
                 </div>
             </div>
             <div class='vehicle-info' v-if='vehicleData.isOnVehicle || vehicleData.isCompassOn'>
@@ -59,7 +59,7 @@
                     </div>
                 </div>
                 <div class='bottom'>
-                    <div class='direction'><div class='image' v-bind:style='{ transform: "translate3d(" + vehicleData.direction + "px, 0px, 0px)" }'></div></div>
+                    <div class='direction'><div class='image' :style='{ transform: "translate3d(" + vehicleData.direction + "px, 0px, 0px)" }'></div></div>
                     <p v-if='vehicleData.isOnVehicle'>{{ vehicleData.location }}</p>
                 </div>
             </div>
@@ -85,7 +85,7 @@
                 return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 			},
 			convertNumber: function(number) {
-				return ((100 - Number(number)) + '%');
+				return ((100 - parseInt(number)) + '%');
 			}
 		}
     };
