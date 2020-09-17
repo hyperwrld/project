4ff8aa3534f8b84492e630c027fba7d7fdf9e387
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-09-17 03:48:43 +0100

    [feat]: cleaned up crp-shops

--- resources/[cacemrp]/crp-shops/fxmanifest.lua
+++ resources/[cacemrp]/crp-shops/fxmanifest.lua
@@ -1,11 +1,19 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 
 game 'gta5'
 
+dependecies {
+	'PolyZone',
+	'crp-ui',
+	'crp-lib'
+}
+
 client_scripts {
+	'@PolyZone/client/main.lua',
+	'@PolyZone/client/BoxZone.lua',
+	'@PolyZone/client/CircleZone.lua',
+	'@PolyZone/client/ComboZone.lua',
 	'client/main.lua'
 }
 
-server_scripts {
-	'server/main.lua'
-}
+shared_script '@crp-lib/shared/util.lua'
