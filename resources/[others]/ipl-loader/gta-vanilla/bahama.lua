b739b184389f8e9d9484b6b321604243775827cd
Tiago Guerreiro <tiagoguerreiro1999@outlook.pt>
2020-10-02 22:28:42 +0100

    [feat]: removed old scripts

--- resources/[others]/ipl-loader/gta-vanilla/bahama.lua
+++
@@ -1,9 +0,0 @@
--- Bahama Mamas: (-1388.0013, -618.41967, 30.819599)
-
-BahamaMamas = {
-    ipl = 'hei_sm_16_interior_v_bahama_milo_',
-
-    Enable = function(state)
-        EnableIpl(BahamaMamas.ipl, state)
-    end
-}
--- resources/[others]/ipl-loader/gta-vanilla/red-carpet.lua
+++
@@ -1,4 +0,0 @@
-RedCarpet = {
-	ipl = 'redCarpet',
-	Enable = function(state) EnableIpl(RedCarpet.ipl, state) end
-}
