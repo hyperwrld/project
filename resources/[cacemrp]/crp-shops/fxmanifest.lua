53e0662d00e95c1b469e92fd27dbd9d52371698d
Tiago Guerreiro <41821953+orakah@users.noreply.github.com>
2020-10-20 15:39:36 +0100

    [feat]: implemented the new polyzone creation system on crp-shops

--- resources/[cacemrp]/crp-shops/fxmanifest.lua
+++ resources/[cacemrp]/crp-shops/fxmanifest.lua
@@ -2,18 +2,8 @@ fx_version 'cerulean'
 
 game 'gta5'
 
-dependecies {
-	'PolyZone',
-	'crp-ui',
-	'crp-lib'
-}
+dependecie 'crp-lib'
 
 client_scripts {
-	'@PolyZone/client/main.lua',
-	'@PolyZone/client/BoxZone.lua',
-	'@PolyZone/client/CircleZone.lua',
-	'@PolyZone/client/ComboZone.lua',
 	'client/main.lua'
 }
-
-shared_script '@crp-lib/shared/util.lua'
