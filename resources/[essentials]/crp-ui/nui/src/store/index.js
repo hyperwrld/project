743f8528013f5aa425a8138fa7d072c7ceeaac82
orakah <41821953+orakah@users.noreply.github.com>
2020-11-15 20:19:13 +0000

    [feat]: simplified the store autoloader

--- resources/[essentials]/crp-ui/nui/src/store/index.js
+++ resources/[essentials]/crp-ui/nui/src/store/index.js
@@ -1,7 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import modules from './modules';
+import camelCase from 'lodash/camelCase';
+
+const files = require.context('.', true, /\.js$/), modules = {};
+
+files.keys().forEach(fileName => {
+  	if (fileName === './index.js') {
+		return;
+	}
+
+	const moduleName = camelCase(fileName.match(/[ \w-]+?(?=\.)/)[0]);
+
+  	modules[moduleName] = files(fileName).default;
+});
 
 Vue.use(Vuex);
 
