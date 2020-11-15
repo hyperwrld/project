570efe64c103e9f17cc9752cbd53f08ee7c830f8
orakah <41821953+orakah@users.noreply.github.com>
2020-11-15 17:04:51 +0000

    [feat]: small improvements on the currency component

--- resources/[essentials]/crp-ui/nui/src/components/interface/currency/currency.vue
+++ resources/[essentials]/crp-ui/nui/src/components/interface/currency/currency.vue
@@ -16,13 +16,21 @@
 		render (h) {
 			return (
 				<div class='currency'>
-					<div class='money'>
-						<span>€ </span>{ formatNumber(currencyData.currentMoney) }
-					</div>
-					{ currencyData.canShow && currencyData.transactionStatus &&
-						<div class='inner-container'>
-							<span class={ currencyData.transactionType }>{ (currencyData.transactionType == 'add' ? '+' : '-') + ' €'}</span> { currencyData.transactionQuantity }
-						</div>
+					{ this.currencyData.canShow &&
+						<transition name='fadeInOut' appear>
+							<div class='inner-container'>
+								<div class='money'>
+									<span>€ </span>{ this.formatNumber(this.currencyData.currentMoney) }
+								</div>
+								{ this.currencyData.transactionStatus &&
+									<transition name='fade' appear>
+										<div class='transaction'>
+											<span class={ this.currencyData.transactionType }>{ (this.currencyData.transactionType == 'add' ? '+' : '-') + ' €'}</span> { this.currencyData.transactionQuantity }
+										</div>
+									</transition>
+								}
+							</div>
+						</transition>
 					}
 				</div>
 			);
