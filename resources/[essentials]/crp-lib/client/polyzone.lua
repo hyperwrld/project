f3875fe6212bbb71f306206916efed8140cae06a
orakah <41821953+orakah@users.noreply.github.com>
2020-12-15 19:20:55 +0000

    [refactor]: support more zones types on crp-lib (polyzone module)

--- resources/[essentials]/crp-lib/client/polyzone.lua
+++ resources/[essentials]/crp-lib/client/polyzone.lua
@@ -1,19 +1,23 @@
-function createCircleZones(points, zoneName, resourceName)
+function createZones(type, points, zoneName, eventName) -- type: 1 (PolyZones) / 2 (CircleZones)
 	local zones = {}
 
 	for i = 1, #points do
-		name = 'circle-' .. tostring(i)
+		name = 'zone-' .. tostring(i)
 
-		zones[#zones + 1] = CircleZone:Create(points[i].coords, 0.6, { name = name, useZ = true, data = points[i].data })
+		if type == 1 then
+			zones[#zones + 1] = PolyZone:Create(points[i].coords, 0.6, { name = name, useZ = true, data = points[i].data })
+		elseif type == 2 then
+			zones[#zones + 1] = CircleZone:Create(points[i].coords, 0.6, { name = name, useZ = true, data = points[i].data })
+		end
 	end
 
 	local zone = ComboZone:Create(zones, { name = zoneName })
 
 	zone:onPlayerInOut(function(isPointInside, point, zone)
 		if zone then
-			TriggerEvent(resourceName ..'onPlayerInOut', isInsideZone, point)
+			TriggerEvent(eventName .. ':onPlayerInOut', isInsideZone, point)
 		end
 	end)
 end
 
-exports('createCircleZones', createCircleZones)
+exports('createZones', createZones)
