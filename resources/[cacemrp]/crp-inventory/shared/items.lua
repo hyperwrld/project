49d769a59ba3f6f80ea8bc5f9d89c5adcc0b1bfd
orakah <41821953+orakah@users.noreply.github.com>
2021-01-29 06:37:06 +0000

    [feat]: removed items description

--- resources/[cacemrp]/crp-inventory/shared/items.lua
+++ resources/[cacemrp]/crp-inventory/shared/items.lua
@@ -2,7 +2,6 @@ itemsList = {
 	{
 		identifier = 'CRP_CACETETE',
 		name = 'Cacetete',
-		description = 'Cacetete com apróximadamente 50cm, fabricado em aço e super resistente.',
 		image = 'crp-cacetete.png',
 		hash = 1737195953,
 		weight = 1.5,
@@ -12,7 +11,6 @@ itemsList = {
 	{
 		identifier = 'CRP_TASER',
 		name = 'Taser',
-		description = 'Uma arma de eletrochoque é um dispositivo não-letal capaz de emitir uma descarga elétrica de alta tensão e baixa corrente com o objetivo de provocar dor e afastar um agressor.',
 		image = 'crp-stungun.png',
 		hash = 911657153,
 		weight = 1.5,
@@ -22,7 +20,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA',
 		name = 'Pistola',
-		description = 'Arma de fogo de cano curto usada para uso pessoal em ações de pequeno alcance.',
 		image = 'crp-pistol.png',
 		hash = 453432689,
 		weight = 10,
@@ -32,7 +29,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA_SNS',
 		name = 'Pistola SNS',
-		description = 'SNS é uma sigla para "Saturday Night Special", uma gíria inglesa para armas de fogo pequenas e baratas.',
 		image = 'crp-snspistol.png',
 		hash = -1076751822,
 		weight = 10,
@@ -42,7 +38,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA_VINTAGE',
 		name = 'Pistola Vintage',
-		description = 'Arma de fogo com cano cumprido, tem uma ótima precisão mas tem uma velocidade de fogo lenta devido ao seu recoil.',
 		image = 'crp-vintagepistol.png',
 		hash = 137902532,
 		weight = 10,
@@ -52,7 +47,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA_PESADA',
 		name = 'Pistola Pesada',
-		description = 'Uma arma de fogo .50 apresenta grande semelhança com uma desert eagle e tem um poder imenso.',
 		image = 'crp-heavypistol.png',
 		hash = -771403250,
 		weight = 10,
@@ -62,7 +56,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA_COMBATE',
 		name = 'Pistola de Combate',
-		description = 'É uma arma fogo poderosa, matando alvos com cerca de 2 tiros e se tornando muito eficaz.',
 		image = 'crp-combatpistol.png',
 		hash = 1593441988,
 		weight = 10,
@@ -72,7 +65,6 @@ itemsList = {
 	{
 		identifier = 'CRP_PISTOLA_METRALHADORA',
 		name = 'Pistola-Metralhadora',
-		description = 'Pistola automática tem uma incrível velocidade de fogo, mas não tem uma boa precisão.',
 		image = 'crp-machinepistol.png',
 		hash = -619010992,
 		weight = 10,
@@ -82,7 +74,6 @@ itemsList = {
 	{
 		identifier = 'CRP_MINI_SMG',
 		name = 'Mini Submetralhadora',
-		description = 'Uma pequena sub-metralhadora com uma semelhança da pistola-metraladora mas com uma rapidez de fogo maior.',
 		image = 'crp-minismg.png',
 		hash = -1121678507,
 		weight = 15,
@@ -92,7 +83,6 @@ itemsList = {
 	{
 		identifier = 'CRP_MICRO_SMG',
 		name = 'Micro Submetralhadora',
-		description = 'Uma das sub-metralhadoras mais poderosas do mundo.',
 		image = 'crp-microsmg.png',
 		hash = 324215364,
 		weight = 15,
@@ -102,7 +92,6 @@ itemsList = {
 	{
 		identifier = 'CRP_SMG',
 		name = 'Submetralhadora',
-		description = 'É uma sub-metralhadora relativamente maior e com mais poder de fogo do que as outras.',
 		image = 'crp-smg.png',
 		hash = 736523883,
 		weight = 15,
@@ -112,7 +101,6 @@ itemsList = {
 	{
 		identifier = 'CRP_RIFLE_COMPACTO',
 		name = 'Rifle Compacto',
-		description = 'Um rifle pequeno de cano curto, mas forte, que causa mais dano que o rifle de assalto original em troca de mais recoil.',
 		image = 'crp-compactrifle.png',
 		hash = 1649403952,
 		weight = 20,
@@ -122,7 +110,6 @@ itemsList = {
 	{
 		identifier = 'CRP_RIFLE_ASSALTO',
 		name = 'Rifle de Assalto',
-		description = 'Rifle de assalto com grande capacidade de precisão à longa distância.',
 		image = 'crp-assaultrifle.png',
 		hash = -1074790547,
 		weight = 20,
@@ -132,9 +119,8 @@ itemsList = {
 	{
 		identifier = 'CRP_CARABINA_MK2',
 		name = 'Carabina MK2',
-		description = 'A carabina mais leve que a rifle de assalto e com mais precisa, esta arma normalmente é utilizada pela polícia.',
 		image = 'crp-carbinerifle.png',
-		hash = -2084633992,
+		hash = 4208062921,
 		weight = 20,
 		decayRate = 0.0,
 		canStack = false
@@ -142,7 +128,6 @@ itemsList = {
 	{
 		identifier = 'CRP_COLETE',
 		name = 'Colete',
-		description = 'É um artefato militar ou policial e que tem como objetivo proteger os utilizadores contra projéteis ou destroços.',
 		image = 'crp-colete.png',
 		weight = 14,
 		decayRate = 0.8,
@@ -151,7 +136,6 @@ itemsList = {
 	{
 		identifier = 'CRP_LIGADURA',
 		name = 'Ligadura',
-		description = 'Uma faixa de tecido que recobre uma ferida, geralmente para fixar curativos.',
 		image = 'crp-ligadura.png',
 		weight = 0.75,
 		decayRate = 0.0,
@@ -160,7 +144,6 @@ itemsList = {
 	{
 		identifier = 'CRP_COCACOLA',
 		name = 'Coca-Cola',
-		description = 'Coca-Cola é um refrigerante que muda o sabor da tua vida.',
 		image = 'crp-coca.png',
 		weight = 0.75,
 		decayRate = 0.0,
@@ -169,7 +152,6 @@ itemsList = {
 	{
 		identifier = 'CRP_BATATAS',
 		name = 'Batatas',
-		description = 'Batatas fritas são comumente servidas como petisco ou acompanhamento.',
 		image = 'crp-batatas.png',
 
 		weight = 0.75,
@@ -179,7 +161,6 @@ itemsList = {
 	{
 		identifier = 'CRP_GAZUA',
 		name = 'Gazua',
-		description = 'Ferramenta utilizada para forçar a abertura de fechaduras.',
 		image = 'crp-lockpick.png',
 		weight = 3,
 		decayRate = 0.0,
@@ -203,6 +184,15 @@ itemsList = {
 		decayRate = 0.0,
 		canStack = false,
 		eventName = 'crp-hud:usedWatch'
+	},
+	{
+		identifier = 'CRP_TELEMOVEL',
+		name = 'Telemóvel',
+		description = '',
+		image = 'crp-relogio.png',
+		weight = 0,
+		decayRate = 0.0,
+		canStack = false
 	}
 }
 
