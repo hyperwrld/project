1b8d9a3f1f490b1a74256f9330b0b3536499faac
orakah <41821953+orakah@users.noreply.github.com>
2021-01-31 00:12:37 +0000

    [feat]: better way to create a blip

--- resources/[essentials]/crp-base/client/modules/blips.lua
+++ resources/[essentials]/crp-base/client/modules/blips.lua
@@ -1,21 +1,15 @@
-CRP.Blips, CRP.BlipsType = {}, {
-	[52]  = { name = 'Loja', scale = 0.75, color = 2 },             [60]  = { name = 'Departamento da polícia', scale = 0.75, color = 29 },
-	[61]  = { name = 'Hospital', scale = 0.75, color = 2 },         [71]  = { name = 'Barbearia', scale = 0.75, color = 1 },
-	[73]  = { name = 'Loja de roupa', scale = 0.65, color = 3 },    [75]  = { name = 'Loja de tatuagens', scale = 0.75, color = 1 },
-	[108] = { name = 'Banco', scale = 0.75, color = 52 },           [110] = { name = 'Loja de armas', scale = 0.75, color = 35 },
-	[225] = { name = 'Loja de veículos', scale = 0.75, color = 4 }
-}
+CRP.Blips = {}
 
-function createBlip(blipId, coords, type)
-	local blip, blipData = AddBlipForCoord(coords), CRP.BlipsType[type]
+function createBlip(blipId, coords, type, color, scale, name)
+	local blip = AddBlipForCoord(coords)
 
 	SetBlipSprite(blip, type)
 	SetBlipAsShortRange(blip, true)
-	SetBlipColour(blip, blipData.color)
-	SetBlipScale(blip, blipData.scale)
+	SetBlipColour(blip, color)
+	SetBlipScale(blip, scale)
 
     BeginTextCommandSetBlipName('STRING')
-    AddTextComponentString(blipData.name)
+    AddTextComponentString(name)
 	EndTextCommandSetBlipName(blip)
 
 	CRP.Blips[blipId] = blip
