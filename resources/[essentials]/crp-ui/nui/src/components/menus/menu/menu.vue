999b75e9dd3dc6d07fa845b8d040996e87b10cff
orakah <41821953+orakah@users.noreply.github.com>
2021-02-21 22:30:17 +0000

    [feat]: reworking crp-menu 2/?

--- resources/[essentials]/crp-ui/nui/src/components/menus/menu/menu.vue
+++ resources/[essentials]/crp-ui/nui/src/components/menus/menu/menu.vue
@@ -1,17 +1,15 @@
 <script>
 	import { mapGetters } from 'vuex';
 	import { library } from '@fortawesome/fontawesome-svg-core';
-	import { faRunning, faTimes, faWalking } from '@fortawesome/free-solid-svg-icons';
+	import { faGlobeEurope, faLink, faShieldAlt, faTheaterMasks, faTimes, faWalking } from '@fortawesome/free-solid-svg-icons';
 
-	library.add(faTimes, faWalking, faRunning);
+	library.add(faTimes, faGlobeEurope, faShieldAlt, faWalking, faTheaterMasks, faLink);
 
 	import {
 		getDegreePosition, pointToString,
 		numberToString, resolveLoopIndex, waitForTransitionEnd,
 		nextTick, getIndexOffset, calculateScale
 	} from './util.js';
-
-	import { fragment } from '../../../utils/lib.js';
 
 	export default {
 		name: 'menu',
@@ -27,7 +25,7 @@
 			}
 		},
 		created() {
-			this.radius = 50;
+			this.radius = 45;
 			this.innerRadius = this.radius * 0.32;
 			this.sectorSpace = this.radius * 0.03;
 
@@ -39,52 +37,10 @@
 				layerInfo.inner = false;
 			})
 		},
-		mounted: function () {
-			document.addEventListener('keydown', this.keyDown.bind(this));
-			document.addEventListener('wheel', this.mouseWheel.bind(this));
-		},
-		beforeDestroy: function () {
-			document.removeEventListener('keydown', this.keyDown);
-			document.removeEventListener('wheel', this.mouseWheel);
-		},
 		methods: {
-			mouseWheel: function(event) {
-				let delta = -event.deltaY;
-
-				if (delta > 0) {
-					this.selectDelta(1);
-				} else {
-					this.selectDelta(-1);
-				}
-			},
-			keyDown: function(event) {
-				switch (event.key) {
-					case 'Escape':
-					case 'Backspace':
-						this.centerClick();
-						break;
-					case 'Enter':
-						this.menuClick(this.getSelectedItem());
-						break;
-					case 'ArrowRight':
-                    case 'ArrowUp':
-						this.selectDelta(1);
-						break;
-					case 'ArrowLeft':
-                    case 'ArrowDown':
-						this.selectDelta(-1);
-						break;
-					default:
-						break;
-				}
-
-				event.preventDefault();
-			},
 			createMenuLayer: function(items, level) {
 				let info = {
-					inner: true, outer: false,
-					level: level, levelItems: items,
-					sectors: [], selectedIndex: 0
+					inner: true, outer: false, level: level, levelItems: items, sectors: []
 				};
 
 				let sectorCount = Math.max(items.length, 3);
@@ -134,34 +90,6 @@
 			},
 			getCurrentMenu: function() {
 				return this.openMenuList[this.openMenuList.length - 1];
-			},
-			sectorHover: function(event) {
-				if (event.target.parentNode.classList.contains('sector')) {
-					let dataset = event.target.parentNode.dataset;
-
-					if (dataset.itemIndex !== undefined) {
-						let currentMenu = this.getCurrentMenu();
-
-						currentMenu.selectedIndex = parseInt(dataset.itemIndex);
-					}
-				}
-			},
-			getSelectedItem: function() {
-				let currentMenu = this.getCurrentMenu();
-
-				return currentMenu.levelItems[currentMenu.selectedIndex];
-			},
-			selectDelta: function(indexDelta) {
-				let currentMenu = this.getCurrentMenu();
-				let selectedIndex = currentMenu.selectedIndex + indexDelta;
-
-				if (selectedIndex < 0) {
-					selectedIndex = currentMenu.levelItems.length + selectedIndex;
-				} else if (selectedIndex >= currentMenu.levelItems.length) {
-					selectedIndex -= currentMenu.levelItems.length;
-				}
-
-				currentMenu.selectedIndex = selectedIndex;
 			},
 			menuClick: function(item) {
 				if (item.items) {
@@ -276,28 +204,24 @@
 								<svg id={ 'level' + menu.level } class={ `container ${ menu.inner ? 'inner' : '', menu.outer ? 'outer' : '' }` } viewBox='-50 -50 100 100'>
 									{ menu.sectors.map((sector, index) => {
 										return (
-											<fragment>
-												<g
-													class={{ sector: sector.id, dummy: !sector.id, selected: menu.selectedIndex == sector.itemIndex }}
-													data-id={ sector.id } data-index={ index } transform={ sector.transform } data-item-index={ sector.itemIndex }
-												>
-													<path d={ sector.d }/>
-													{ sector.label &&
-														<text text-anchor='middle' font-size='38%' x={ sector.centerX } y={ sector.centerY } transform={ sector.textTransform }>
-															{ sector.label }
-														</text>
-													}
-													<use href={ sector.icon } x={ sector.centerX } y={ sector.centerY } transform={ sector.useTransform } width='10' height='10' fill='white'/>
-												</g>
-												{ index == 0 &&
-													<g class='center'>
-														<circle r={ menu.centerRadius }/>
-														<use href={ menu.centerIcon } transform={ menu.centerTransform } width={ menu.centerSize } height={ menu.centerSize }/>
-													</g>
+											<g
+												class={{ sector: sector.id, dummy: !sector.id }}
+												data-id={ sector.id } data-index={ index } transform={ sector.transform } data-item-index={ sector.itemIndex }
+											>
+												<path d={ sector.d }/>
+												{ sector.label &&
+													<text text-anchor='middle' font-size='38%' x={ sector.centerX } y={ sector.centerY } transform={ sector.textTransform }>
+														{ sector.label }
+													</text>
 												}
-											</fragment>
+												<use href={ '#' + sector.id } x={ sector.centerX } y={ sector.centerY } transform={ sector.useTransform } width='10' height='10' fill='white'/>
+											</g>
 										)
 									})}
+									<g class='center'>
+										<circle r={ menu.centerRadius }/>
+										<use href={ menu.centerIcon } transform={ menu.centerTransform } width={ menu.centerSize } height={ menu.centerSize }/>
+									</g>
 								</svg>
 							)
 						})}
