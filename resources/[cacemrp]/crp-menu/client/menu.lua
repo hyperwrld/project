40fb718c60c322bc0bab84fbdce1f743fd712051
orakah <41821953+orakah@users.noreply.github.com>
2021-02-22 17:38:00 +0000

    [feat]: implemented open function

--- resources/[cacemrp]/crp-menu/client/menu.lua
+++ resources/[cacemrp]/crp-menu/client/menu.lua
@@ -1,5 +1,56 @@
 function openMenu()
-	exports['crp-ui']:openApp('menu')
+	local data = {}
+
+	for k, v in ipairs(menuList) do
+		if v:enableSector() then
+			local elements, hasSubMenu = {}, false
+
+			if v.subMenus and #v.subMenus > 0 then
+				local previous, element = elements, {}
+
+				hasSubMenu = true
+
+				for i = 1, #v.subMenus do
+					element[#element + 1] = v.subMenus[i]
+
+					if i % 5 == 0 and i < (#v.subMenus - 1) then
+						previous[6] = {
+							id = 'more',
+							label = 'Mais...',
+							icon = 'ellipsis-h',
+							items = element
+						}
+
+						previous, element = element, {}
+					end
+				end
+
+				if #element > 0 then
+					previous[6] = {
+						id = 'more',
+						label = 'Mais...',
+						icon = 'ellipsis-h',
+						items = element
+					}
+				end
+
+				elements = elements[6].items
+			end
+
+			data[#data + 1] = {
+				id = v.id,
+				label = v.label,
+				icon = v.icon,
+				functionName = v.functionName
+			}
+
+			if hasSubMenu then
+				data[#data].items = elements
+			end
+		end
+	end
+
+	exports['crp-ui']:openApp('menu', data)
 end
 
 RegisterCommand('+openMenu', openMenu, false)
--- resources/[cacemrp]/crp-menu/client/util.lua
+++ resources/[cacemrp]/crp-menu/client/util.lua
@@ -1,29 +1,90 @@
 menuList = {
 	{
-		id: 'general',
-		label: 'Geral',
-		icon: 'globe-europe'
+		id = 'general',
+		label = 'Geral',
+		icon = 'globe-europe',
+		enableSector = function()
+			return true
+		end,
+		subMenus = {
+			{
+				id = 'animations',
+				label = 'Animações',
+				icon = 'meh-blank',
+				functionName = ''
+			},
+			{
+				id = 'give-keys',
+				label = 'Dar chaves',
+				icon = 'car',
+				functionName = ''
+			},
+			{
+				id = 'check-car',
+				label = 'Examinar o veículo',
+				icon = 'diagnoses',
+				functionName = ''
+			},
+			{
+				id = 'escort',
+				label = 'Agarrar',
+				icon = 'user-friends',
+				functionName = ''
+			},
+			{
+				id = 'put-in-vehicle',
+				label = 'Colocar no veículo',
+				icon = 'sign-in-alt',
+				functionName = ''
+			},
+			{
+				id = 'unseat-vehicle',
+				label = 'Retirar do veículo',
+				icon = 'fa-sign-out-alt',
+				functionName = ''
+			}
+		}
 	},
 	{
-		id: 'police-actions',
-		label: 'Funções da polícia',
-		icon: 'shield-alt'
+		id = 'police-actions',
+		label = 'Funções da polícia',
+		icon = 'shield-alt',
+		enableSector = function()
+			return true
+		end
 	},
 	{
-		id: 'animations',
-		label: 'Estilos de andar',
-		icon: 'walking'
+		id = 'walking-style',
+		label = 'Estilos de andar',
+		icon = 'walking',
+		enableSector = function()
+			return true
+		end
 	},
 	{
-		id: 'expressions',
-		label: 'Expressões',
-		icon: 'theater-masks'
+		id = 'expressions',
+		label = 'Expressões',
+		icon = 'theater-masks',
+		enableSector = function()
+			return true
+		end
 	},
 	{
-		id: 'cuff',
-		label: 'Algemar/Desalgemar',
-		icon: 'link',
-		functionName: 'crp-police:cuff'
+		id = 'vehicle',
+		label = 'Veículo',
+		icon = 'car',
+		enableSector = function()
+			return true
+		end
+	},
+	{
+		id = 'cuff',
+		label = 'Algemar/Desalgemar',
+		icon = 'link',
+		enableSector = function()
+			return true
+		end,
+		functionName = 'crp-police:cuff'
 	}
 }
 
