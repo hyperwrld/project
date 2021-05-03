04b4c121b967b127389e5fe426197f8ddea0cd3a
Tiago Guerreiro <41821953+orakah@users.noreply.github.com>
2021-05-03 19:20:36 +0100

    [feat]: ability to disableShoot while doing a task

--- resources/[essentials]/crp-ui/client/modules/taskbar.lua
+++ resources/[essentials]/crp-ui/client/modules/taskbar.lua
@@ -1,25 +1,37 @@
-local isDoingAction, wasCancelled, percentage = false, false, 0
+local isDoingAction, wasCancelled, canCancelTask, percentage = false, false, false, 0
 
-function setTaskbar(message, time, canCancel)
+function setTaskbar(message, time, canCancel, disableShoot)
 	if isDoingAction then
 		setAlert('Não é possível fazer duas ações ao mesmo tempo.', 'error')
 
 		return false
 	end
 
-	isDoingAction, wasCancelled = true, false
+	isDoingAction, wasCancelled, canCancelTask = true, false, canCancel
 
 	SendNUIMessage({
 		app = 'taskbar', event = 'setTaskbar', data = { message = message, time = time }
 	})
 
-	while isDoingAction do
+	while isDoingAction and (disableShoot) do
 		Citizen.Wait(0)
 
-		if IsControlJustPressed(0, 200) and canCancel then
-			SendNUIMessage({
-				app = 'taskbar', event = 'stopTaskbar'
-			})
+		if disableShoot then
+			DisablePlayerFiring(PlayerId(), true) -- Disable weapon firing
+
+			DisableControlAction(0, 24, true) -- Disable attack
+			DisableControlAction(0, 25, true) -- Disable aim
+			DisableControlAction(1, 37, true) -- Disable weapon select
+			DisableControlAction(0, 47, true) -- Disable weapon
+			DisableControlAction(0, 58, true) -- Disable weapon
+
+			DisableControlAction(0, 140, true) -- Disable melee
+			DisableControlAction(0, 141, true) -- Disable melee
+			DisableControlAction(0, 142, true) -- Disable melee
+			DisableControlAction(0, 143, true) -- Disable melee
+			DisableControlAction(0, 263, true) -- Disable melee
+			DisableControlAction(0, 264, true) -- Disable melee
+			DisableControlAction(0, 257, true) -- Disable melee
 		end
 	end
 
@@ -29,6 +41,8 @@ function setTaskbar(message, time, canCancel)
 
 	return true
 end
+
+exports('setTaskbar', setTaskbar)
 
 RegisterNUICallback('finishedTask', function(data, cb)
     isDoingAction, wasCancelled = false, false
@@ -42,4 +56,12 @@ RegisterNUICallback('canceledTask', function(data, cb)
     cb(true)
 end)
 
-exports('setTaskbar', setTaskbar)
+function cancelTask()
+	if canCancelTask then
+		SendNUIMessage({
+			app = 'taskbar', event = 'stopTaskbar'
+		})
+	end
+end
+
+exports['crp-binds']:RegisterKeybind('taskbar', '[Taskbar] Cancelar ação', 'ESC', cancelTask)
